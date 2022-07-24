dcb8385b8851a8ef7d28f5ac3db707e030bc177a
koemanqiu@sanhua <koeman.qiu@gmail.com>
2022-07-24 15:32:48 +0800

    优化代码

--- MinHeap.lua
+++ MinHeap.lua
@@ -1,65 +1,86 @@
+--Lua工具 最小堆
+
+--[[
+simple example:
+local MH = require "KMQ/MinHeap"
+local mh = MH()
+mh.Push(10)
+mh.Push(3)
+mh.Push(2)
+mh.Push(1)
+
+print(mh.Pop())
+print(mh.Pop())
+print(mh.Pop())
+]]--
+
 local function MinHeap()
   local this = {}
   
+  --privte methods
+  local ShiftUp
+  local SinkDown
+  
+  --private data
+  local data = {}
+  local size = 0
+  
   function this.init()
-      this._lbData = {};
-      this._dataLen = 0;
+
       return this
   end
 
-  function this.push(num)
-      table.insert( this._lbData, num);
-      this._dataLen = this._dataLen + 1;
-      this.sortHeap(#this._lbData); 
+  function this.Push(num)
+      table.insert( data, num)
+      size = size + 1
+      ShiftUp(#data)
+  end
+  
+  function this.Pop()
+    if size <= 0 then
+      return nil
+    end
+    
+    local ret = data[1]
+    local endNum = table.remove(data,size)
+    size = size - 1
+    data[1] = endNum
+    SinkDown(1)
+    return ret
   end
 
-  function this.sortHeap(nIndex)
+  --private
+  function ShiftUp(nIndex)
       if nIndex <= 1 then
-          if nIndex ~= 1 then
-              print("sort error")
-          end
-          return true;
+          return true
       end
-      local fIndex;
-      if nIndex%2 == 0 then
-          fIndex = nIndex/2;
+      local parentIdx = nIndex // 2
+
+      if data[nIndex] < data[parentIdx] then
+          data[nIndex],data[parentIdx] = data[parentIdx],data[nIndex]
+          return ShiftUp(parentIdx)
       else
-          fIndex = (nIndex-1)/2;
-      end
-      if this._lbData[nIndex] < this._lbData[fIndex] then
-          this._lbData[nIndex],this._lbData[fIndex] = this._lbData[fIndex],this._lbData[nIndex];
-          return this.sortHeap(fIndex);
-      else
-          return true;
+          return true
       end
   end
 
-  function this.remove()
-      local ret = this._lbData[1];
-      local endNum = table.remove(this._lbData,this._dataLen);
-      this._dataLen = this._dataLen - 1;
-      this._lbData[1] = endNum;
-      this.sortHeap2(1);    
-      return ret;
-  end
-
-  function this.sortHeap2(nIndex)
-      local cIndex = nIndex*2;
-      if cIndex <= this._dataLen and this._lbData[cIndex] < this._lbData[nIndex] then
-          this._lbData[nIndex],this._lbData[cIndex] = this._lbData[cIndex],this._lbData[nIndex];
-          this.sortHeap2(cIndex);
-          cIndex = nIndex*2+1;
-          if this._lbData[cIndex] < this._lbData[nIndex] then
-              this._lbData[nIndex],this._lbData[cIndex] = this._lbData[cIndex],this._lbData[nIndex];
-              this.sortHeap2(cIndex)
-          end
-      end
-      cIndex = nIndex*2+1;
-      if cIndex <= this._dataLen and this._lbData[cIndex] < this._lbData[nIndex] then
-          this._lbData[nIndex],this._lbData[cIndex] = this._lbData[cIndex],this._lbData[nIndex];
-          this.sortHeap2(cIndex);
-      end
-      return true;
+  function SinkDown(nIndex)
+    
+    local leftIdx = nIndex * 2
+    local rightIdx = nIndex * 2 + 1    
+    local targetIdx = leftIdx
+    
+    if rightIdx <= size and data[rightIdx] < data[leftIdx] then
+      targetIdx = rightIdx
+    elseif leftIdx > size then
+      return true
+    end
+    
+    if data[nIndex] > data[targetIdx] then
+      data[nIndex],data[targetIdx] = data[targetIdx],data[nIndex]
+      SinkDown(targetIdx)
+    end
+    return true
   end
 
   return this.init()
